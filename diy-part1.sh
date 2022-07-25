5374d15ae7ed4fe6e0d98beaf41cba5c212158a4
wlfit80 <wlfit80@gmail.com>
2022-07-25 19:21:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2/tree/main/luci-app-passwall2' >>feeds.conf.default
+echo 'src-git ikoolproxy https://github.com/yaof2/luci-app-ikoolproxy' >>feeds.conf.default
